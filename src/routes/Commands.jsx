1b21117a549c0a1fe231a9b900a64fbdca9e40eb
gabixdev <gabixdeveloper@gmail.com>
2019-03-17 04:00:10 +0100

    command filtering + some fixes

--- src/routes/Commands.jsx
+++ src/routes/Commands.jsx
@@ -1,65 +1,16 @@
-import React, {Component, Fragment} from 'react';
+import React, {Fragment} from 'react';
 import Hero from "../components/Hero";
-import {Col, Row, UncontrolledTooltip} from 'reactstrap';
+import {Col, Input, UncontrolledTooltip} from 'reactstrap';
 import Scrollspy from 'react-scrollspy';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-
-const commands = {
-    "Basic": [
-        {
-            "name": "help",
-            "description": "List of all available commands."
-        },
-        {
-            "name": "invite",
-            "description": "Invite Sagiri to your guild."
-        },
-        {
-            "name": "test",
-            "usage": "test <gay|asd|foo|bar> [user]",
-            "description": "blah"
-        },
-        {
-            "name": "test5345",
-            "usage": "test lol [lol|a|b]",
-            "description": "blah"
-        }
-    ],
-    "Moderation": [
-        {
-            "name": "ban",
-            "usage": "ban <user> [reason]",
-            "description": "Throw a ban hammer at someone."
-        },
-        {
-            "name": "test1234",
-            "description": "blah"
-        },
-        {
-            "name": "test4234",
-            "description": "blah"
-        },
-        {
-            "name": "test34532",
-            "description": "blah"
-        },
-        {
-            "name": "test123",
-            "description": "blah"
-        },
-        {
-            "name": "test234",
-            "description": "blah"
-        }
-    ]
-};
+import commands from '../commands.json';
 
 const uuidv4 = () => '_xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
 });
 
-const usageToReact = usage => {
+const usageToReact = (usage) => {
     let elements = [];
 
     let splits = usage.split(" ");
@@ -99,81 +50,114 @@ const usageToReact = usage => {
     return elements;
 };
 
-class Sidebar extends React.PureComponent {
-    render() {
-        let items = [];
-        let links = [];
+const Sidebar = (props) => {
+    let items = [];
+    let links = [];
 
-        Object.keys(this.props.commands).forEach(cmd => {
-            if (cmd) {
-                items.push(cmd);
-                links.push(<li key={"sidebar-" + cmd} className={"sidebar-link"}><AnchorLink offset="-150"
-                                                                                             href={`#${cmd}`}>{cmd}</AnchorLink>
-                </li>);
+    Object.keys(props.commands).forEach(cmd => {
+        if (cmd) {
+            items.push(cmd);
+            links.push(<li key={"sidebar-" + cmd} className={"sidebar-link"}><AnchorLink offset="-150"
+                                                                                         href={`#${cmd}`}>{cmd}</AnchorLink>
+            </li>);
+        }
+    });
+
+    return (
+        <Col className="d-none d-sm-block commands-sidebar" md={3}>
+            <Scrollspy items={items} currentClassName="active">
+                {links}
+            </Scrollspy>
+        </Col>
+    );
+};
+
+const CommandCard = (props) => {
+    return (
+        <div className="commands-card">
+            <h5>{props.command.name}</h5>
+            {props.command.description}<br/>
+            {props.command.usage
+                ? (<Fragment>{"Usage: "}{usageToReact(props.command.usage)}</Fragment>)
+                : (<Fragment>{"Usage: "}<span
+                    className="usage-command">{props.command.name}</span></Fragment>)}
+        </div>
+    );
+};
+
+const CommandCategory = (props) => {
+    let elements = [];
+    props.commands.forEach(c => elements.push(<CommandCard key={`card_${props.name}_${c.name}`} command={c}/>));
+
+    return (
+        <section id={props.name} className="commands-category">
+            <h2>{props.name}</h2>
+            {elements}
+        </section>
+    );
+};
+
+class CommandList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            filter: null
+        };
+    }
+
+    updated(e) {
+        const value = e.target.value.trim();
+
+        if (value.length === 0) {
+            this.setState({filter: null});
+        } else {
+            this.setState({filter: value});
+        }
+    }
+
+    render() {
+        let categories = [];
+        Object.keys(commands).forEach(cat => {
+            if (commands[cat].length !== 0) {
+                let newCmds;
+
+                if (this.state.filter !== null) {
+                    const filt = this.state.filter.toLowerCase();
+                    newCmds = commands[cat].filter(cmd =>
+                        (cmd.name && cmd.name.toLowerCase().includes(filt))
+                        || (cmd.description && cmd.description.toLowerCase().includes(filt)));
+                } else {
+                    newCmds = commands[cat];
+                }
+
+                if (newCmds.length !== 0) {
+                    categories.push(<CommandCategory key={"category_" + cat} name={cat} commands={newCmds}/>)
+                }
             }
         });
 
         return (
-            <Col className="commands-sidebar" md={3}>
-                <Scrollspy items={items} currentClassName="active">
-                    {links}
-                </Scrollspy>
+            <Col sm={12} md={9}>
+                <div className="commands-searchbox clearfix">
+                    <Input placeholder="Filter commands" onChange={this.updated.bind(this)}/>
+                </div>
+                {categories}
             </Col>
         );
     }
 }
 
-class CommandCard extends React.PureComponent {
-    render() {
-        return (
-            <div className="commands-card">
-                <h5>{this.props.command.name}</h5>
-                {this.props.command.description}<br/>
-                {this.props.command.usage
-                    ? (<Fragment>{"Usage: "}{usageToReact(this.props.command.usage)}</Fragment>)
-                    : (<Fragment>{"Usage: "}<span
-                        className="usage-command">{this.props.command.name}</span></Fragment>)}
-            </div>
-        );
-    }
-}
-
-class CommandCategory extends React.PureComponent {
-    render() {
-        let elements = [];
-        this.props.commands.forEach(c => elements.push(<CommandCard key={`card_${this.props.name}_${c.name}`}
-                                                                    command={c}/>));
-
-        return (
-            <section id={this.props.name} className="commands-category">
-                <h2>{this.props.name}</h2>
-                {elements}
-            </section>
-        );
-    }
-}
-
-class Commands extends Component {
-    render() {
-        let categories = [];
-        Object.keys(commands).forEach(cat => {
-            categories.push(<CommandCategory key={"category_" + cat} name={cat} commands={commands[cat]}/>)
-        });
-
-        return (
-            <Fragment>
-                <Hero>
-                    <h1>COMMANDS</h1>
-                </Hero>
-                <div className="d-flex">
-                    <Sidebar commands={commands}/>
-                    <Col md={9}>
-                        {categories}
-                    </Col>
-                </div>
-            </Fragment>
-        )
-    }
-}
+const Commands = () => (
+    <Fragment>
+        <Hero>
+            <h1>COMMANDS</h1>
+        </Hero>
+        <div className="d-flex">
+            <Sidebar commands={commands}/>
+            <CommandList/>
+        </div>
+    </Fragment>
+);
 
 export default Commands;
