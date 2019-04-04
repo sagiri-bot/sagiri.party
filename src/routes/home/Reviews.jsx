1e1b45c8db478e5938f259e3acf7d9e1118b0fd2
kiru <itskiru@protonmail.com>
2019-04-04 18:33:52 +0200

    add some real reviews

--- src/routes/home/Reviews.jsx
+++ src/routes/home/Reviews.jsx
@@ -19,12 +19,11 @@ class Reviews extends React.PureComponent {
                     <h1 className="mt-5">Trusted by over a thousand of Discord servers</h1>
                     <h5>See what the people say about Sagiri</h5>
                     <Row>
-                        <Review by="aaaaaa" avatar="https://cdn.discordapp.com/avatars/219067402174988290/a_6b61800643369365f5c806aff385dfd3.png?size=256">
-                            There might be the same Discord bot out there but this one blah blah blah...
+                        <Review by="alula" avatar="https://files.gang.moe/4uG3kzbi.png">
+                            Sagiri is our little server helper, she contains features that help us keeping server clean.
                         </Review>
-                        <Review by="ophis" avatar="https://cdn.discordapp.com/avatars/199217346911404032/a_6bf037cf605d9f3dfae2608acf72ebed.png?size=256">
-                            Guys btw there's this really queer person called amy
-                            and like she's a java programmer lol
+                        <Review by="shu" avatar="https://files.gang.moe/ZGXBOR0B.png">
+                            I am using this bot on my 3.8k members server and it's a really big help for us.
                         </Review>
                         <Review by="anime girl" avatar="https://t4.rbxcdn.com/3b6ff3d387f8bfd902489514a4608fca">
                             Sagiri big cute OwO
